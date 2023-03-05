cb31680d0057c3eaf9837d79337099684195d5af
ndFrontender <108065561+ndigor@users.noreply.github.com>
2023-03-05 13:31:13 +0300

    Create like-button.js

---
+++ like-button.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+
+  if (liked) {
+    return 'You liked this!';
+  }
+
+  return React.createElement(
+    'button',
+    {
+      onClick: () => setLiked(true),
+    },
+    'Like'
+  );
+}
